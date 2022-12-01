31855223d85aee3096d869dc1a92e01129199e7d
raphaelfiorda <raphaelfiorda@hotmail.com>
2022-12-01 05:23:27 -0300

    feat: Função digitalRoot() em js

---
+++ javascript/6kyu.js
@@ -0,0 +1,17 @@
+// Função que retorna a raiz digital de dado número "n"
+function digitalRoot(n) {
+  toNumber = (str) => parseInt(str, 10); // função auxiliar
+  toStringList = (int) => int.toString().split(""); // função auxiliar
+
+  // result iniciará como a soma recursiva de seus dígitos
+  result = toStringList(n).reduce((acc, el) => acc + toNumber(el), 0);
+
+  // enquanto obtiver mais de um digíto, repete a soma recursiva
+  while (toStringList(result).length > 1) {
+    result = toStringList(result).reduce((acc, el) => acc += toNumber(el), 0);
+  }
+
+  return result;
+}
+
+console.log(digitalRoot(1));
