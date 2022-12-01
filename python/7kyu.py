33fb66c10d45cc492f9f28d35900869a117f1c9f
raphaelfiorda <raphaelfiorda@hotmail.com>
2022-12-01 04:21:50 -0300

    refactor: Refatora maskify()

--- python/7kyu.py
+++ python/7kyu.py
@@ -1,5 +1,4 @@
 # A função maskify() substitui todos os caracteres, menos os ultimos quatro, para "#"
 def maskify(string):
     index_cap = len(string) - 4 if len(string) > 4 else 0
-    return "".join(['#' for i in range(index_cap)]) + string[index_cap:]
-        
+    return index_cap * '#' + string[index_cap:]
