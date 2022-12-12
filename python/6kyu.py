ba8a44df122e629a0d2948f2703c8ecb45d52ca8
raphaelfiorda <raphaelfiorda@hotmail.com>
2022-12-12 18:47:41 -0300

    feat: Conclui a implementação de array_diff()

--- python/6kyu.py
+++ python/6kyu.py
@@ -1,3 +1,7 @@
 # A função array_diff() subtrai uma lista pela outra e retorna o resultado
 def array_diff(a, b):
-    pass
+    return [el for el in a
+            if not b.count(el)]
+
+
+print(array_diff([3, 5, 2, 5, 1, 1], [1, 3, 2, 2, 4, 9]))
